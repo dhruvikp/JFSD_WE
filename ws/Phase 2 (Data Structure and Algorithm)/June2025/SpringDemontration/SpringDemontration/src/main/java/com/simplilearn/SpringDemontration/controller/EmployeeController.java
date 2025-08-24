ce2e713b4450096910b4e397513c29d00fc3d003
Dhruvik Parikh <dhruviksparikh@gmail.com>
2025-08-24 21:35:45 +0530

    Initial commit

--- ws/Phase 2 (Data Structure and Algorithm)/June2025/SpringDemontration/SpringDemontration/src/main/java/com/simplilearn/SpringDemontration/controller/EmployeeController.java
+++ ws/Phase 2 (Data Structure and Algorithm)/June2025/SpringDemontration/SpringDemontration/src/main/java/com/simplilearn/SpringDemontration/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.simplilearn.SpringDemontration.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import com.simplilearn.SpringDemontration.service.EmployeeService;
 public class EmployeeController {
 
 	@Autowired
+//	@Qualifier("employeeService")
 	private EmployeeService employeeService;
 	
 	@GetMapping("/employees")
--- ws/Phase 2 (Data Structure and Algorithm)/June2025/SpringDemontration/SpringDemontration/src/main/java/com/simplilearn/SpringDemontration/service/EmployeeService.java
+++ ws/Phase 2 (Data Structure and Algorithm)/June2025/SpringDemontration/SpringDemontration/src/main/java/com/simplilearn/SpringDemontration/service/EmployeeService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import com.simplilearn.SpringDemontration.model.Employee;
 import com.simplilearn.SpringDemontration.repository.EmployeeRepository;
 
-@Service
+@Service()
 public class EmployeeService {
 	
 	@Autowired
